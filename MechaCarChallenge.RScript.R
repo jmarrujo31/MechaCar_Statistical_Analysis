215b56cad3741546a9f8169c9cf0ddf52d1d0c04
jmarrujo31 <89328181+jmarrujo31@users.noreply.github.com>
2021-12-05 07:34:00 -0800

    f

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,12 @@
+library(jsonlite)
+library(tidyverse)
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+
+
+
+
+
+
+#deliverable2
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
